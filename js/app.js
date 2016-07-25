03ab6ce2459fc46d0f326ef65bd3ca77c9a9f455
Cathleen Carbonell <catcarbonell@gmail.com>
2016-07-24 19:21:47 -0700

    Installing ARKHIVES v1

---
+++ js/app.js
@@ -0,0 +1,60 @@
+// ANGULAR MVC //
+
+var app = angular.module("theApp", ["ngRoute", "angular-parallax"]);
+
+//var parallax = angular.module("parallax", ["angular-parallax"]);
+
+// The ARKHIVES Main Views
+app.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider
+    .when("/", {
+	templateUrl: "/main.htm"
+      })
+
+      .when("/about", {
+	templateUrl: "/about.htm"
+      })
+
+      .when("/jnj", {
+	templateUrl: "/stories/jnj/index.html"
+      })
+
+      .when("/sample", {
+	templateUrl: "/stories/sample/index.html"
+      })
+
+      .otherwise({
+	redirectTo: "/"
+      });
+
+}]);
+
+
+// SEARCH FUNCTION w/ ANGULAR //
+//Service to connect to the "storiesdb" JSON file
+app.service("storiesService", function ($http, $q)
+	{
+		var deferred = $q.defer();
+		$http.get('js/storiesdb.json').then(function (data)
+		{
+			deferred.resolve(data); 
+		});
+
+		this.getStories = function ()
+		{
+			return deferred.promise; 
+		}
+	})
+
+//Controller to display the data inside storiesdb.json
+.controller("MainCtrl", function ($scope, storiesService)
+	{
+		var promise = storiesService.getStories();
+		promise.then(function (data)
+			{
+				$scope.stories = data.data;
+
+			});
+
+	})
